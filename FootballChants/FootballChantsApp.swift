0bcf4e0d8935475dc591e0fb9ead06d3f4b2176b
CharlieNguyen94 <charlietugnuyen@hotmail.com>
2021-09-04 12:43:28 +0100

    Initial Commit

---
+++ FootballChants/FootballChantsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballChantsApp.swift
+//  FootballChants
+//
+//  Created by Charlie Nguyen on 04/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct FootballChantsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
