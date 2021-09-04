e6b6dcdf5c866231dcd4509b06cb0ec7f61f0364
CharlieNguyen94 <charlietugnuyen@hotmail.com>
2021-09-04 16:57:43 +0100

    Completed layout for team row view.

--- FootballChants/ContentView.swift
+++ FootballChants/ContentView.swift
@@ -12,24 +12,42 @@ struct ContentView: View {
     let team: Team
     
     var body: some View {
-        VStack(alignment: .leading) {
-            
-            HStack(alignment: .top, spacing: 12) {
+        List(/*@START_MENU_TOKEN@*/0 ..< 5/*@END_MENU_TOKEN@*/) { item in
+            VStack(alignment: .leading) {
                 
-                Image(team.id.badge)
-                
-                VStack(alignment: .leading, spacing: 5) {
-                    Text(team.name)
-                    Text("Founded: \(team.founded)")
-                    Text("Manager")
-                    Text(team.info)
+                HStack(alignment: .top, spacing: 12) {
                     
+                    Image(team.id.badge)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 50, height: 50)
+                    
+                    VStack(alignment: .leading, spacing: 5) {
+                        Text(team.name)
+                            .font(.system(size: 18, weight: .bold))
+                        Text("Founded: \(team.founded)")
+                            .font(.system(size: 12, weight: .light))
+                        Text("Manager")
+                            .font(.system(size: 10, weight: .light))
+                        Text(team.info)
+                            .font(.system(size: 12, weight: .medium))
+                        
+                    }
+                    
+                    Spacer()
+        
+                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                        Image(systemName: "play.circle.fill")
+                            .resizable()
+                            .scaledToFit()
+                    })
+                    .frame(maxWidth: 40, maxHeight: .infinity, alignment: .center)
                 }
-    
-                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-                    Image(systemName: "play.circle.fill")
-                })
             }
+            .padding(8)
+            .foregroundColor(.white)
+            .background(team.id.background)
+            .cornerRadius(10)
         }
     }
     
